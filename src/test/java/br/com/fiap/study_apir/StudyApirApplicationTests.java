0064e3e6c4e9627a6bddb9637c86c6441bb78b36
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-02 20:24:52 -0300

    feat: setup inicial

---
+++ src/test/java/br/com/fiap/study_apir/StudyApirApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.study_apir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
